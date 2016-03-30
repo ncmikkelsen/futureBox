ab69d0823e5260b2cd153ab32da6871f64083169
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2016-03-30 16:27:45 +0200

    Switch ready
    Threw all the code for printing in its own function. Added code to
    support 5 physical switches that enables turning on/off parts of the
    prompt. Created to vars to hold the size of the categories and their
    contents.

--- futureBox/futureBox.ino
+++ futureBox/futureBox.ino
@@ -15,12 +15,33 @@ String terrainList[] = {"agriculture", "the brain", "childhood", "citizenship", 
 String objectList[] = {"advertisement", "artwork", "beverage", "book", "bottle", "box", "brochure", "building", "candy", "clothing", "corporation", "device", "document", "event", "festival", "flag", "game", "gift", "headline", "implant", "instrument", "jewellery", "kit", "law", "logo", "lotion", "machine", "magazine cover", "map", "mask", "monument", "passport", "pill", "plant", "postcard", "poster", "product", "prosthetic", "public service announcement", "relic", "ritual", "show", "slogan", "snack", "song", "souvenir", "statue", "sticker", "symbol", "t-shirt", "tattoo", "tool", "toy", "vehicle", "video", "weapon", "wildcard (artifact of your choice)"};
 String moodList[] = {"admiration", "alienation", "amusement", "anger", "anxiety", "awkwardness", "calm", "charm", "cheer", "contentment", "curiosity", "decadence", "delight", "dignity", "disgust", "dread", "embarrassment", "excitement", "exhilaration", "fascination", "fervor", "frustration", "gratitude", "happiness", "hilarity", "hope", "longing", "malaise", "melancholy", "melodrama", "nostalgia", "optimism", "outrage", "pathos", "pleasure", "pride", "rationality", "relief", "resentment", "respect", "sadness", "satisfaction", "serenity", "shame", "shock", "sorrow", "surprise", "unease", "warmth", "weirdness", "wellbeing", "wonder", "worry", "zen"};
 
+// The physical switches goes from these to a ground on the middle leg of the switch
+int arc1Switch = 22;
+int arc2Switch = 23;
+int terrainSwitch = 24;
+int objectSwitch = 25;
+int moodSwitch = 26;
+
+// Button goes from here to ground
 int button = 3;
 boolean lastButton = HIGH;
 
+// This var decides how many empty lines are printed after the prompt. Change according to the box the printer is put in
+int endPadding = 4;
+
+// These two vars sets the size of the category names and the contents. 'L' for large, 'M' for medium and 'S' for small
+char categorySize = 'M';
+char contentSize = 'S';
+
 void setup() {
+  pinMode(arc1Switch, INPUT_PULLUP);
+  pinMode(arc2Switch, INPUT_PULLUP);
+  pinMode(terrainSwitch, INPUT_PULLUP);
+  pinMode(objectSwitch, INPUT_PULLUP);
+  pinMode(moodSwitch, INPUT_PULLUP);
+
   pinMode(button, INPUT_PULLUP);
-  Serial.begin(9600);
+
 
   mySerial.begin(19200);  // Initialize SoftwareSerial
   printer.begin();        // Init printer (same regardless of serial
@@ -29,56 +50,67 @@ void setup() {
 void loop() {
   int currentButton = digitalRead(button);
   if (!currentButton && lastButton) {
-    String arc1 = arc1List[random(4)];
-    String arc2 = arc2List[random(6)];
-    String terrain = terrainList[random(57)];
-    String object = objectList[random(57)];
-    String mood = moodList[random(54)];
-    Serial.println("Arc");
-    Serial.println(arc1);
-    Serial.println(arc2);
-    Serial.println();
-    Serial.println("Terrain");
-    Serial.println(terrain);
-    Serial.println();
-    Serial.println("Object");
-    Serial.println(object);
-    Serial.println();
-    Serial.println("Mood");
-    Serial.println(mood);
-    Serial.println();
-
-    printer.println("");
-    
-    printer.boldOn();
-    printer.println("Arc");
-    printer.boldOff();
-    
-    printer.println(arc1);
-    printer.println(arc2);
-    printer.println();
-    
-    printer.boldOn();
-    printer.println("Terrain");
-    printer.boldOff();
-
-    printer.println(terrain);
-    printer.println();
-    
-    printer.boldOn();
-    printer.println("Object");
-    printer.boldOff();
-    
-    printer.println(object);
-    printer.println();
-    
-    printer.boldOn();
-    printer.println("Mood");
-    printer.boldOff();
-    
-    printer.println(mood);
-    printer.println(" ");
-
+    printText();
   };
   lastButton = currentButton;
+}
+
+void printText() {
+  String arc1 = arc1List[random(4)];
+  String arc2 = arc2List[random(6)];
+  String terrain = terrainList[random(57)];
+  String object = objectList[random(57)];
+  String mood = moodList[random(54)];
+
+  boolean arc1State = digitalRead(arc1Switch);
+  boolean arc2State = digitalRead(arc2Switch);
+  boolean terrainState = digitalRead(terrainSwitch);
+  boolean objectState = digitalRead(objectSwitch);
+  boolean moodState = digitalRead(moodSwitch);
+
+  if (arc1State || arc2State) {
+    printer.setSize(categorySize);
+    printer.println("Arc");
+    printer.setSize(contentSize);
+    if (arc1State) {
+      printer.println(arc1);
+    }
+    if (arc2State) {
+      printer.println(arc2);
+    }
+  }
+
+  if (terrainState) {
+    printer.feed(1);
+
+    printer.setSize(categorySize);
+    printer.println("Terrain");
+    printer.setSize(contentSize);
+
+    printer.println(terrain);
+  }
+
+  if (objectState) {
+    printer.feed(1);
+    printer.setSize(categorySize);
+    printer.println("Object");
+    printer.setSize(contentSize);
+    printer.println(object);
+  }
+
+  if (moodState) {
+    printer.feed(1);
+    printer.setSize(categorySize);
+    printer.println("Mood");
+    printer.setSize(contentSize);
+    printer.println(mood);
+  }
+
+  if (!arc1State && !arc2State && !terrainState && !objectState && !moodState) {
+    printer.setSize('M');
+    printer.println("There is no future");
+  }
+  printer.feed(endPadding);
+
+
 }
